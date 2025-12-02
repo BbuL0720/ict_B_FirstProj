79de241a33242ed205f1ddd240fb6d58886a50dc
BbuL0720 <llsm4680@gmail.com>
2025-12-02 10:42:00 +0900

    App.tsx 라우터 구조 수정

--- ictb/src/App.tsx
+++ ictb/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import BoardList from './pages/board/BoardList';
-import Header from './pages/Header'
-import Login from './pages/login/Login';
-
+import React from "react";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import BoardList from "./pages/board/BoardList";
+import Header from "./pages/Header";
+import Login from "./pages/login/Login";
 
 function App() {
+  let isLogin = true;
   return (
-<Router>
- <Header/>
-   <Routes>
-    {/* 로그인 세션이 있으면 */}
-    if(true) ( 
-     <Route path='/' element={<BoardList/>} />
-     )else{
-      <Route path='/' element={<Login/>} />
-     }
-    
-  </Routes>
-</Router>
+    <Router>
+      {isLogin && <Header />}
+      <Routes>
+        <Route
+          path="/"
+          element={isLogin ? <BoardList /> : <Navigate to="/login" replace />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
