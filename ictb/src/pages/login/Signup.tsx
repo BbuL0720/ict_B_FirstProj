45ecc54e65da3ef88dc0bc7222210dec510004ee
pyt55 <pyt9923@gmail.com>
2025-12-15 19:20:01 +0900

    회원가입 최종본

--- ictb/src/pages/login/Signup.tsx
+++ ictb/src/pages/login/Signup.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import style from "./signup.module.css"
-import { light } from '@mui/material/styles/createPalette';
+
 const Signup: React.FC = () => {
-  const AuthClick = () => useState('');
+  
+  
   return (
     
     <div className={style.wrapper} style={{backgroundColor:'#f8f9fa'}}>
-      <div className={style.container}>
-        <div className={style.h1}>
-        <h1><strong>나만의 캠퍼스</strong></h1>
-        </div>
+      <div className={style.container}>     
+          <div className='tag'>
+        <h1><img src='img/logo.png' style={{width:'75px',height:'auto'}}></img><strong style={{margin:'10px'}}>나만의 캠퍼스</strong></h1>
+        </div>       
         <form className={style.form}>     
             <h2 className="form-title">회원가입</h2>
             <div className="mb-4">
@@ -19,6 +20,7 @@ const Signup: React.FC = () => {
             <div className="mb-4">
                 <label>아이디</label>
                 <input type="id" className="form-control" placeholder="아이디를 입력해주세요"/>
+                <button type='button' className='btn btn-primary w-30' style={{float:'right',marginTop:'5px'}}>중복확인</button>
             </div>
              <div className="mb-4">
                 <label>비밀번호</label>
@@ -28,15 +30,13 @@ const Signup: React.FC = () => {
             <div className="mb-5">
                 <label>이메일</label>              
                 <input type="email" className="form-control" placeholder="이메일을 입력해주세요"/>
-                <button type='submit' className='btn btn-primary w-30' style={{float:'right', marginTop:'5px'}}>인증하기</button>         
+                <button type='submit'className='btn btn-primary w-30' style={{float:'right', marginTop:'5px'}}>인증하기</button>                   
             </div>
             <div className="mb-5">
                 <label>인증번호</label>              
-                <input type="hidden" className="form-control" onClick={AuthClick}/>
-                <button type='submit' className='btn btn-primary w-30' style={{float:'right', marginTop:'5px'}}>확인</button>            
+                <input type="text" className="form-control" placeholder="인증번호를 입력해주세요"/>
+                <button type='submit'className='btn btn-primary w-30' style={{float:'right', marginTop:'5px'}}>확인</button>                   
             </div>
-            
-            
             <button type="submit" className="btn btn-primary w-100">가입하기</button>
         </form>
         
