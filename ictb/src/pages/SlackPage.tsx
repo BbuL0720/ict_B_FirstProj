6e62e81137a2489226756bd1ab0b3fd2ad527620
jmixmuzik <kjaemin45@gmail.com>
2025-12-15 17:50:08 +0900

    게시판 호버효과 적용

--- ictb/src/pages/SlackPage.tsx
+++ ictb/src/pages/SlackPage.tsx
@@ -5,6 +5,8 @@ import AutoAwesomeMosaicIcon from "@mui/icons-material/AutoAwesomeMosaic";
 import LostFound from "./lostfound/LostFound";
 import LostList from './lost/LostList';
 import LostForm from './lost/LostForm';
+import style from "./lost.module.css";
+import { hover } from "@testing-library/user-event/dist/hover";
 import { Link } from "react-router-dom";
 
 interface LayoutProps {
@@ -57,17 +59,19 @@ const SlackPage: React.FC<LayoutProps> = ({ children }) => {
 
             <div className="section-title mt-4">일반 게시판</div>
             <div className="nav flex-column">
-              <a className="nav-link active"># 자유게시판</a>
-              <a className="nav-link"># 정보 공유</a>
-              <a className="nav-link"># 홍보 게시판</a>
-              <a className="nav-link"># 동아리</a>
+
+              {/* <a href="#" ># 자유게시판</a> */}
+              <a className="nav-link text-hover" ># 자유게시판</a>
+              <a className="nav-link text-hover" ># 정보 공유</a>
+              <a className="nav-link text-hover"># 홍보 게시판</a>
+              <a className="nav-link text-hover"># 동아리</a>
             </div>
 
             <div className="section-title mt-4">학사 게시판</div>
             <div className="nav flex-column">
-              <a className="nav-link"># 캠퍼스 공지사항</a>
-              <a className="nav-link"># 학과별 공지사항</a>
-              <a className="nav-link"># 건의사항</a>
+              <a className="nav-link text-hover"># 캠퍼스 공지사항</a>
+              <a className="nav-link text-hover"># 학과별 공지사항</a>
+              <a className="nav-link text-hover"># 건의사항</a>
             </div>
           </div>
 
