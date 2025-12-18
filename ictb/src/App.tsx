09bf839eb573091905f542f28e9f131da90735f8
ktg950228-cmyk <ktg950228@naver.com>
2025-12-18 15:26:03 +0900

    Route 및 Diary

--- ictb/src/App.tsx
+++ ictb/src/App.tsx
@@ -11,7 +11,6 @@ import BoardList from "./pages/board/BoardList";
 import Header from "./pages/Header";
 import Login from "./pages/login/Login";
 import Todo from "./pages/todo/Todo";
-import SlackPage from "./pages/SlackPage";
 import LostList from "./pages/lost/LostList";
 import Undo from "./pages/undo/Undo";
 import Signup from "./pages/login/Signup";
@@ -19,9 +18,10 @@ import BoardDetail from "./pages/board/BoardDetail";
 import BoardForm from "./pages/board/BoardForm";
 import LostForm from "./pages/lost/LostForm";
 import LostDetail from "./pages/lost/LostDetail";
+import SlackPage from "./pages/SlackPage";
 
 function App() {
-  let isLogin = false; //axios로 로그인 정보 받고 데이터가 있으면? 으로 처리해야할듯(세션) 아마 안될듯
+  let isLogin = true; //axios로 로그인 정보 받고 데이터가 있으면? 으로 처리해야할듯(세션) 아마 안될듯
 
   return (
     <Router>
@@ -30,13 +30,17 @@ function App() {
         <SlackPage>
           <Routes>
             <Route path="/" element={<Navigate to="/board/list/1" replace />} />
+
+            <Route path="/board/:kind/list" element={<BoardList />} />
+            <Route path="/board/:kind/detail/:id" element={<BoardDetail />} />
+            <Route path="/board/:kind/form" element={<BoardForm />} />
+
             <Route path="/todo" element={<Todo />} />
-            <Route path="/board/list/:id" element={<BoardList />} />
-            <Route path="/board/detail/:id" element={<BoardDetail />} />
-            <Route path="/board/form" element={<BoardForm />} />
-            <Route path="/lost/list/:id" element={<LostList />} />
-            <Route path="/lost/form" element={<LostForm />} />
-            <Route path="/lost/detail/:id" element={<LostDetail />} />
+
+            <Route path="/lost/:kind/list" element={<LostList />} />
+            <Route path="/lost/:kind/detail/:id" element={<LostDetail />} />
+            <Route path="/lost/:kind/form" element={<LostForm />} />
+
             <Route path="*" element={<Undo />} />
           </Routes>
         </SlackPage>
--- ictb/src/pages/SlackPage.tsx
+++ ictb/src/pages/SlackPage.tsx
@@ -1,125 +1,90 @@
 import React, { useEffect, useState } from "react";
 import AutoAwesomeMosaicIcon from "@mui/icons-material/AutoAwesomeMosaic";
-import { Link, useLocation } from "react-router-dom";
-import Header from "./Header";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 interface LayoutProps {
-  children: React.ReactNode;
+	children?: React.ReactNode;
 }
-interface LayoutTab {
-  title: string,
-  param: string,
-  subTitle: subTitle[]
+
+interface Title {					//메인 아이템(한개)
+	title: string;
+	url_main: string;
+	subTitles?: SubTitle[];
 }
-interface subTitle {
-  id: number,
-  param: string,
-  subtitle: string
+
+interface SubTitle {				//세부 아이템(한개)
+	subTitle: string;
+	url_sub: string;
 }
+
+const sideItems: Title[] = [		//본격 더미데이터(메인 아이템들)
+	{
+		title: '게시판',
+		url_main: 'board',
+		subTitles: [{ subTitle: '자유 게시판', url_sub: 'free/list' },
+		{ subTitle: '정보 게시판', url_sub: 'info/list' },
+		{ subTitle: '홍보 게시판', url_sub: 'Prom/list' }]
+	},
+	{
+		title: 'ToDo',
+		url_main: 'todo'
+	},
+	{
+		title: '분실물',
+		url_main: 'lost',
+		subTitles: [{ subTitle: '분실물 발생', url_sub: 'lost/list' },
+		{ subTitle: '분실물 습득', url_sub: 'find/list' }]
+	}
+]
 
 const SlackPage: React.FC<LayoutProps> = ({ children }) => {
- 
-  const board = { title: '게시판', param: '/board/list', subTitle: [{ id: 1, param: '/1', subtitle: '자유 게시판' }, { id: 2, param: '/2', subtitle: '정보 공유' }, { id: 1, param: '/3', subtitle: '홍보 게시판' }] }
-  const todo = { title: 'TODO', subTitle: [{ id: 1, subtitle: '자유 게시판' }, { id: 1, subtitle: '정보 공유' }, { id: 1, subtitle: '홍보 게시판' }] }
-  const lost = { title: '찾아용', param: '/lost/list', subTitle: [{ id: 1, param: '/1', subtitle: '잃어버렸어요' }, { id: 1, param: '/2', subtitle: '발견했어요' }] }
-   const [title, setTitle] = useState<LayoutTab>(board)
-  const location = useLocation();
-  // useEffect(() => {
-  //   console.log(location)
-  //   setTitle(board)
-  // }, [])
 
+	const locArray = useLocation().pathname.split('/');
+	const url_main = locArray[1];
+	const url_sub = locArray[2];
 
-  useEffect(() => {
-    const path = location.pathname.split("/")[1];
+	return (
+		<div>
+			<div>
+				<div className="slack-container d-flex" style={{ position: "fixed", width: "100%", height: "100vh" }}>
+					<div className="workspace-bar d-flex flex-column align-items-center justify-content-between" style={{ width: '80px' }}>
+						<div className="d-flex flex-column">
+							{
+								sideItems.map((e) => (
+									<Link to={`/${e.url_main}${e.subTitles ? `/${e.subTitles[0].url_sub}` : ''}`} className="no-style">
+										<span className="workspace-icon" style={{ color: "#d7c4daff" }}>
+											<AutoAwesomeMosaicIcon />
+										</span>
+										<p className="nanum-gothic-regular" style={{ fontFamily: "Paperozi", fontSize: "13px" }}>{e.title}</p>
+									</Link>
+								))
+							}
+						</div>
+						<div className="p-1 mb-4" style={{ fontFamily: "Paperozi", fontSize: "13px" }}>
+							<Link to="/logout">로그아웃</Link>
+						</div>
+					</div>
 
-    if (path === "board") {
-      console.log("board");
-      setTitle(board)
-    } else if (path === "lost") {
-      setTitle(lost)
-    }
-  }, [location.pathname]);
+					{
+						sideItems.map((e) => ( e.url_main === url_main ? 
+							<div className="sidebar p-3 nanum-gothic-regular">
+								<div className="workspace-title">{e.title}</div>
+								<div className="nav flex-column">
+									{e.subTitles?.map((ee) => (
+										<Link to={`/${e.url_main}/${ee.url_sub}`} className="nav-link text-hover">{ee.subTitle}</Link>
+									))}
+								</div>
+							</div> : <></>
+						))
+					}
 
-  // const changeTItle = (id: string) => {
-  //   if (id == '게시판') {
-  //     setTitle(board)
-  //   } else if (id == 'TODO') {
-  //     setTitle(null)
-  //   } else if (id == '찾아용') {
-  //     setTitle(lost)
-  //   }
-  // }
-
-  return (
-    <div>
-      <div>
-        <div className="slack-container d-flex" style={{ position: "fixed", width: "100%", height: "100vh" }}>
-          <div className="workspace-bar d-flex flex-column align-items-center justify-content-between">
-            <div className="d-flex flex-column ">
-              <Link to="/board/list/1" className="no-style" >
-                <span className="workspace-icon" style={{ color: "#d7c4daff" }}>
-                  <AutoAwesomeMosaicIcon />
-                </span>
-                <p
-                  className="nanum-gothic-regular"
-                  style={{ fontFamily: "Paperozi", fontSize: "13px" }}
-                >
-                  게시판
-                </p>
-              </Link>
-              <Link to="/todo" className="no-style" >
-                <span className="workspace-icon" style={{ color: "#d7c4daff" }}>
-                  <AutoAwesomeMosaicIcon />
-                </span>
-                <p
-                  className="nanum-gothic-regular"
-                  style={{ fontFamily: "Paperozi", fontSize: "13px" }}
-                >
-                  ToDo
-                </p>
-              </Link>
-              <Link to="/lost/list/1" className="no-style" >
-                <span className="workspace-icon" style={{ color: "#d7c4daff" }}>
-                  <AutoAwesomeMosaicIcon />
-                </span>
-                <p
-                  className="nanum-gothic-regular"
-                  style={{ fontFamily: "Paperozi", fontSize: "13px" }}
-                >
-                  찾아용
-                </p>
-              </Link>
-            </div>
-            <div className="p-1 mb-4" style={{ fontFamily: "Paperozi", fontSize: "13px" }}> <Link to="/logout">로그아웃</Link></div>
-          </div>
-
-          {title && title.title != "TODO" && (
-            <div className="sidebar p-3 nanum-gothic-regular">
-              <div className="workspace-title"
-              // onClick={ changeTItle("게시판")}
-              >{title.title}</div>
-
-              {title.subTitle.map((e) => (
-                <div className="nav flex-column">
-
-                  {/* <a href="#" ># 자유게시판</a> */}
-                  <Link to={`${title.param}${e.param}`} className="nav-link text-hover" ># {e.subtitle}</Link>
-                </div>
-              ))}
-            </div>
-          )
-          }
-
-
-          <div style={{ flex: 1, overflow: "auto" }}>
-            <Header title={title?.title}/>
-            {children}      
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div style={{ flex: 1, overflow: "auto" }}>
+						{children}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SlackPage;
